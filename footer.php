04a0b12855ac6d77bc8441111ab626f820832203
Ahmed-arkan <33808871+Ahmed-arkan@users.noreply.github.com>
2018-03-10 17:13:24 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+ 	<!-- closing div all from navbar -->
+  	</div> 
+
+
+	<!-- copyright -->
+	  <footer class="footer-section" style="background-color: rgba(0, 0, 0, 0.7); color: rgba(244, 244, 244, 0.7);">
+        <div class="container">
+            <div class="row">
+               
+      <div class="col-md-4 text-left">
+      
+       <p><span><a href="http://www.codefactory.academy/ahmed-arkan/" class="smoth-scroll">About Me</a></span> | <span><a href="http://www.codefactory.academy/ahmed-arkan/" class="smoth-scroll">Portfolio</a></span></p>
+          </div>
+               
+            <div class="col-md-4 text-center">
+            	<br>
+               <p>© Copyright 2016 Ahmed|Events.</p>
+               </div>
+              
+             <div class="col-md-4 uipasta-credit text-right">
+                <p>Design By <a href="http://www.codefactory.academy/ahmed-arkan/" target="_blank" title="UiPasta">Ahmed </a></p>
+                </div>
+                
+             </div>
+        </div>
+    </footer>
+
+
+ 	<!-- bootstrap -->
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
